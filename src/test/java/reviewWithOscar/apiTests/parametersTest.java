d8e4e09d3befa9bb5d2f0328e06ba2bc16217df4
OscarSah <oscar@cybertekschool.com>
2022-03-20 17:26:26 +0300

    API Review Zip Example

--- src/test/java/reviewWithOscar/apiTests/parametersTest.java
+++ src/test/java/reviewWithOscar/apiTests/parametersTest.java
@@ -4,6 +4,7 @@ import static io.restassured.RestAssured.*;
 import static org.testng.Assert.*;
 
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
 
@@ -42,5 +43,28 @@ And body should contains following information
         assertEquals(response.statusCode(),200);
         assertEquals(response.contentType(),"application/json");
 
+      //  And Server header is cloudflare
+        assertTrue(response.header("Server").equalsIgnoreCase("cloudflare"));
+     //   And Report-To header exists
+        assertTrue(response.headers().hasHeaderWithName("Report-To"));
+
+        // post code is 22031
+        assertEquals(response.path("\'post code\'"),"22031");
+        assertEquals(response.path("country"),"United States");
+
+        // country abbreviation is US
+        assertEquals(response.path("\'country abbreviation\'"),"US");
+        // place name is Fairfax
+        assertEquals(response.path("places[0].\'place name\'"),"Fairfax");
+        // second way
+        assertEquals(response.path("places.\'place name\'[0]"),"Fairfax");
+
+        // JSONPATH method to verify body
+        // state is Virginia
+        JsonPath jsonPath = response.jsonPath();
+        assertEquals(jsonPath.getString("places[0].state"),"Virginia");
+
+        // latitude is 38.8604
+        assertEquals(jsonPath.getString("places[0].latitude"),"38.8604");
     }
 }
